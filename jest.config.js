fb777364af47ebcdebb3533cd1ca9d9d4de0252e
Andrei Filashov <rayinaway@gmail.com>
2023-02-05 05:09:38 +0300

    test: implement SCSS module mocking

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
 	testMatch: ['<rootDir>/src/**/*.test.js'],
 	moduleNameMapper: {
 		'^.*/assets/.*$': '<rootDir>/test/mocks/asset.js',
+		'^.+\\.scss$': 'identity-obj-proxy',
 		'^~/(.*)$': '<rootDir>/src/$1'
 	},
 	testEnvironment: 'jsdom',
