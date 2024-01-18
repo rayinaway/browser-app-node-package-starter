be4f77c1e5f5e4ee3492015c7ac9f5dd5920a569
Andrei Filashov <rayinaway@gmail.com>
2024-01-18 13:38:02 +0300

    style: fix code style in postbuild script file

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -2,7 +2,7 @@ const Fs = require('fs');
 const Module = require('module');
 const Path = require('path');
 
-const server = requireWithoutStyleModules('../build/server/server.js');
+const server = requireWithoutStyleModules('../build/server/server');
 
 const RELATIVE_APP_DOCUMENT_FILE_PATH = '../build/index.html';
 const RELATIVE_SERVER_BUILD_DIR_PATH = '../build/server';
@@ -17,29 +17,29 @@ function prerenderApp() {
 		RELATIVE_APP_DOCUMENT_FILE_PATH
 	);
 
-	const serverBuildDirPath = Path.resolve(
-		__dirname,
-		RELATIVE_SERVER_BUILD_DIR_PATH
-	);
-
 	let stringifiedAppDocument = Fs.readFileSync(appDocumentFilePath);
 
-	const appElementApproximatePosition = stringifiedAppDocument.indexOf(
+	const approximateAppElementPosition = stringifiedAppDocument.indexOf(
 		`id="${APP_ELEMENT_ID}"`
 	);
-	const appElementContentMountPosition = stringifiedAppDocument.indexOf(
+	const appContentMountPosition = stringifiedAppDocument.indexOf(
 		'</',
-		appElementApproximatePosition
+		approximateAppElementPosition
 	);
 
 	const stringifiedAppRender = server.renderStringifiedApp();
 
 	stringifiedAppDocument =
-		stringifiedAppDocument.slice(0, appElementContentMountPosition) +
+		stringifiedAppDocument.slice(0, appContentMountPosition) +
 		stringifiedAppRender +
-		stringifiedAppDocument.slice(appElementContentMountPosition);
+		stringifiedAppDocument.slice(appContentMountPosition);
 
 	Fs.writeFileSync(appDocumentFilePath, stringifiedAppDocument);
+
+	const serverBuildDirPath = Path.resolve(
+		__dirname,
+		RELATIVE_SERVER_BUILD_DIR_PATH
+	);
 
 	Fs.rmSync(serverBuildDirPath, {
 		force: true,
@@ -50,15 +50,17 @@ function prerenderApp() {
 function requireWithoutStyleModules(moduleNameOrPath) {
 	const requireOriginal = Module.prototype.require;
 
-	Module.prototype.require = function (moduleNameOrPath) {
-		if (!moduleNameOrPath.endsWith('.css')) {
-			return requireOriginal.call(this, moduleNameOrPath);
-		}
-	};
+	Module.prototype.require = requireOverriden;
 
-	const importedModule = require(moduleNameOrPath);
+	const module = require(moduleNameOrPath);
 
 	Module.prototype.require = requireOriginal;
 
-	return importedModule;
+	return module;
+
+	function requireOverriden(moduleNameOrPath) {
+		if (!moduleNameOrPath.endsWith('.css')) {
+			return requireOriginal.call(this, moduleNameOrPath);
+		}
+	}
 }
