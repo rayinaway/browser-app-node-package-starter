bf742359c1c55e850545d6e3637d3597156f8303
Andrei Filashov <rayinaway@gmail.com>
2024-03-25 21:26:09 +0400

    style: fix code style in markup shell util test file

--- src/shell/utils/markup.test.js
+++ src/shell/utils/markup.test.js
@@ -2,20 +2,9 @@ import {cnx} from './markup';
 
 describe('cnx shell util', () => {
 	test('returns the space-separated concatenation of non-empty string arguments', () => {
-		const className = cnx(
-			undefined,
-			'string1',
-			null,
-			'string2',
-			true,
-			'string3',
-			1,
-			'',
-			[],
-			{}
-		);
+		const className = cnx(undefined, 'a', null, 'b', true, 'c', 1, '', [], {});
 
-		expect(className).toBe('string1 string2 string3');
+		expect(className).toBe('a b c');
 	});
 
 	test('returns undefined if no non-empty strings were passed', () => {
