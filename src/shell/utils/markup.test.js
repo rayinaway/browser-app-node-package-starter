38d09b574912f7b6cad72ab50715be4e9b8575fc
Andrei Filashov <rayinaway@gmail.com>
2024-03-25 21:25:31 +0400

    test: remove redundant cnx shell util test

--- src/shell/utils/markup.test.js
+++ src/shell/utils/markup.test.js
@@ -1,7 +1,7 @@
 import {cnx} from './markup';
 
 describe('cnx shell util', () => {
-	test('ignores non-string and empty string arguments', () => {
+	test('returns the space-separated concatenation of non-empty string arguments', () => {
 		const className = cnx(
 			undefined,
 			'string1',
@@ -14,12 +14,6 @@ describe('cnx shell util', () => {
 			[],
 			{}
 		);
-
-		expect(className).toBe('string1 string2 string3');
-	});
-
-	test('returns the space-separated concatenation of the passed strings', () => {
-		const className = cnx('string1', 'string2', 'string3');
 
 		expect(className).toBe('string1 string2 string3');
 	});
