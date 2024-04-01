feea618bf2a80e093f7a5fa9c4b07556991e2c55
Andrei Filashov <rayinaway@gmail.com>
2024-04-01 15:12:54 +0400

    test: fix incorrect internal path alias resolving

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
 	moduleNameMapper: {
 		'\\.css\\.ts$': '@vanilla-extract/jest-transform',
 		'/assets/': '<rootDir>/test/mocks/asset.js',
-		'^~/(.*)$': '<rootDir>/src/$1'
+		'^~/(.*)$': '<rootDir>/$1'
 	},
 	testEnvironment: 'jsdom',
 	setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
