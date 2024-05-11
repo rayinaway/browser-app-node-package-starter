ac43d7b0bb1488edac5b8af27a0383519669ba02
Andrei Filashov <rayinaway@gmail.com>
2024-05-11 18:18:29 +0300

    style: fix code style in Jest configuration file

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-	testMatch: ['<rootDir>/src/**/*.test.js'],
+	testEnvironment: 'jsdom',
+	setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
+	restoreMocks: true,
+	coverageDirectory: '<rootDir>/test-coverage',
 	moduleNameMapper: {
 		'\\.css\\.ts$': '@vanilla-extract/jest-transform',
 		'/assets/': '<rootDir>/test/mocks/asset.js',
 		'^~/(.*)$': '<rootDir>/$1',
 		'^~~/(.*)$': '<rootDir>/src/$1'
 	},
-	testEnvironment: 'jsdom',
-	setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
-	restoreMocks: true,
-	coverageDirectory: '<rootDir>/test-coverage'
+	testMatch: ['<rootDir>/src/**/*.test.js']
 };
